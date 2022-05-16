99f19c836a752c64a8af4efb3c310382c8b5d4f7
aimer <15267840622@163.com>
2022-05-16 21:16:42 +0800

    1

--- ApiServer/Controller/TestController.php
+++ ApiServer/Controller/TestController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * TestController.php
  * 文件描述
@@ -28,66 +29,73 @@ class TestController extends HttpController
      */
     public function test()
     {
-        return ['data'=>'test'];
+        return ['data' => 'test'];
     }
 
     /**
      * @Action
      * @Route(url="./article/show/{id}",method={"GET"},paramsGet="qq=1")
      */
-    public function artice($id){
-        return ['show'=>$id];
+    public function artice($id)
+    {
+        return ['show' => $id];
     }
 
     /**
      * @Action
      */
-    public function getTest(int $id,string $name){
+    public function getTest(int $id, string $name)
+    {
         return [
-            'id'=>$id,
-            'test'=>$name,
-            '_get'=>$this->request->get('name'),
-            '_post'=>$this->request->post('id',333)
+            'id' => $id,
+            'test' => $name,
+            '_get' => $this->request->get('name'),
+            '_post' => $this->request->post('id', 333)
         ];
     }
 
     /**
      * @Action
      */
-    public function setCookie($name,$value){
-        return $this->response->withCookie($name,$value);
+    public function setCookie($name, $value)
+    {
+        return $this->response->withCookie($name, $value);
     }
 
     /**
      * @Action
      */
-    public function getCookie($name){
-        return ['name'=>$name,'value'=>$this->request->getCookie($name,111)];
+    public function getCookie($name)
+    {
+        return ['name' => $name,'value' => $this->request->getCookie($name, 111)];
     }
 
     /**
      * @Action
      * ImageUrl http://inews.gtimg.com/newsapp_ls/0/14805646356/0
      */
-    public function testHeader(){
-        return $this->response->withHeader("hellofly",$this->request->getHeaderLine('xixi'));
+    public function testHeader()
+    {
+        return $this->response->withHeader("hellofly", $this->request->getHeaderLine('xixi'));
     }
 
     /**
      * @Action
      */
-    public function other(){
-       return [
-           'url'=>$this->request->getUri()->__toString(),
-           'server'=>$this->request->getServerParams()
-       ];
+    public function other(): array
+    {
+        return [
+           'url' => $this->request->getUri()->__toString(),
+           'server' => $this->request->getServerParams()
+        ];
     }
 
     /**
      * @Action
      * @View(renderType="html")
      */
-    public function response_html(){
+    public function response_html()
+    {
         return "hello xiflys";
     }
 
@@ -95,25 +103,23 @@ class TestController extends HttpController
     /**
      * @Action
      */
-    public function upload_demo(){
+    public function upload_demo(): array
+    {
         $uploadedFiles = $this->request->getUploadedFiles();
         $arr = [];
         /**
          * @var UploadedFile $file
          */
-        foreach ($uploadedFiles as $file){
+        foreach ($uploadedFiles as $file) {
             $arr[] = array(
-                'filename'=>$file->getClientFilename(),
-                'media_type'=>$file->getClientMediaType(),
-                'tmp'=>$file->getTmpFilename(),
-                'size'=>$file->getSize(),
-                'size2'=>strlen($file->getStream()->getContents())
+                'filename' => $file->getClientFilename(),
+                'media_type' => $file->getClientMediaType(),
+                'tmp' => $file->getTmpFilename(),
+                'size' => $file->getSize(),
+                'size2' => strlen($file->getStream()->getContents())
             );
         }
 
         return $arr;
     }
-
-
-
 }
